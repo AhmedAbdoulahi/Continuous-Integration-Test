0913b881558b3f8757eb92af61d8dbf568e3de01
AhmedAbdoulahi <abdoulahiahmed8@gmail.com>
2024-09-22 14:34:37 +0200

    format update

--- hello.py
+++ hello.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
+
 def fois(x, y):
     return int(x * y)
+
 
 def div(x, y):
     if y == 0:
         return "Error: Division by zero"
     return int(x // y)
 
+
 def moins(x, y):
     return int(x - y)
 
+
 def add(x, y):
     return int(x + y)
-
-
-
-
